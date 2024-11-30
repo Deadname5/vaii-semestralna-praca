473d1c2f40ee19cda67f63f1ac68e5473cda3d51
Deadname5 <jankus.stehlik@gmail.com>
2024-11-30 21:11:21 +0100

    feat: added students into database

---
+++ docker/sql/ddl.students_01.sql
@@ -0,0 +1,23 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`
+(
+    `id`      int(11)      NOT NULL AUTO_INCREMENT,
+    `jazyk`   char(3) NOT NULL,
+    `zaciatok` date NOT NULL,
+    `koniec` date,
+
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO `student` (`id`, `jazyk`, `zaciatok`, `koniec`)
+VALUES (1, 'ANJ', '2024-6-11', NULL),
+       (2, 'SPJ', '2024-6-18', NULL),
+       (3, 'SPJ', '2024-3-12', '2024-8-12');
